1f98d881c74402fbe0409099d97a21be21dbc2ae
Omoik4ne <56385558+Omoik4ne@users.noreply.github.com>
2021-03-01 11:54:58 +0100

    first commit

---
+++ Exo_Slide_07/EquationSecondDegre.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exo_Slide_07
+{
+    public struct EquationSecondDegre
+    {
+        public double A, B, C;
+
+        public bool Resoudre(out double? X1, out Nullable<double> X2)
+        {
+            X1 = X2 = default;
+            if (A == 0) return false;
+            double delta = (Math.Pow(B, 2)) - (4 * A * C);
+            if (delta < 0) return false;
+            else if (delta == 0)
+            {
+                X1 = X2 = -B / (2 * A);
+                //X1 = -B / (2 * A);
+            }
+            else
+            {
+                X1 = (-B - Math.Sqrt(delta)) / (2 * A);
+                X2 = (-B + Math.Sqrt(delta)) / (2 * A);
+            }
+            return true;
+        }
+    }
+}
---
+++ Exo_Slide_07/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exo_Slide_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EquationSecondDegre eqsd = new EquationSecondDegre();
+            eqsd.A = 4;
+            eqsd.B = 4;
+            eqsd.C = 1;
+
+            double? x1;
+            double? x2;
+
+            if (eqsd.Resoudre(out x1, out x2))
+            {
+                if (x1 == x2)
+                {
+                    Console.WriteLine($"{eqsd.A}X² + {eqsd.B}X + {eqsd.C} = 0 => à une solution {x1}");
+                }
+                else
+                {
+                    Console.WriteLine($"{eqsd.A}X² + {eqsd.B}X + {eqsd.C} = 0 => à deux solutions {x1} et {x2}");
+                }
+            }
+            else {
+                Console.WriteLine($"{eqsd.A}X² + {eqsd.B}X + {eqsd.C} = 0 => n'a pas de solution");
+            }
+        }
+    }
+}
